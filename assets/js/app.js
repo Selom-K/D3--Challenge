8c34880cca6a4bbcf145ad19bd7b9da407683ca3
Selom <selomk@hotmail.com>
2021-05-01 19:44:01 -0500

    Adding chart margins

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,25 @@
 // @TODO: YOUR CODE HERE!
+console.log("app.js loaded");
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = { top: 20, right: 40, bottom: 60, left: 50 };
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// SVG wrapper creation and appending the SVG group to hold the chart
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+  // Chart margins
+  var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+// Chart margins
+  var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
